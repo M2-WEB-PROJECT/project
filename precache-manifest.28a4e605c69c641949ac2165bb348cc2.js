186242f9532c5a025d3f49512b5ff1ced684576b
nujabness <elassouri76>
2020-11-11 16:22:03 +0100

    Updates

---
+++ precache-manifest.28a4e605c69c641949ac2165bb348cc2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5674f6d3756bbd135ef09442d401413f",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "67d945d6dae6e3b8119e",
+    "url": "/project/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "92049b2b6ebf0fd288a2",
+    "url": "/project/static/css/main.5ee3f738.chunk.css"
+  },
+  {
+    "revision": "67d945d6dae6e3b8119e",
+    "url": "/project/static/js/2.d5d0764c.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/project/static/js/2.d5d0764c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92049b2b6ebf0fd288a2",
+    "url": "/project/static/js/main.96894350.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/project/static/js/main.96894350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc99f8f8713a15fe0aa",
+    "url": "/project/static/js/runtime-main.db1d7207.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/project/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/project/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/project/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/project/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/project/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
