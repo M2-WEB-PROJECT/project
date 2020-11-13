59018cf1c6baf0414ab80e7a4b8f888d1029920c
nujabness <elassouri76>
2020-11-13 19:08:57 +0100

    Update

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  publicPath: process.env.NODE_ENV === 'production'? '/project/': '/',
+
+  devServer: {
+    disableHostCheck: true,
+  },
+
+  transpileDependencies: ['vuetify'],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+}
