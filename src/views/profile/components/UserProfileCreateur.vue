3989ed1fe27016cabc1b1af526af5d0da460846b
nujabness <elassouri76>
2020-12-05 14:12:58 +0100

    clean

--- src/views/profile/components/UserProfileCreateur.vue
+++ src/views/profile/components/UserProfileCreateur.vue
@@ -248,8 +248,6 @@
         firestore.collection('users').doc(this.uid).get().then(doc => {
           if (doc.exists) {
             this.setUserData(doc.data())
-          } else {
-            // doc.data() will be undefined in this case
           }
         })
       },
